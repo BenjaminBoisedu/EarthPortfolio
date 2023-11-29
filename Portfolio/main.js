b2c1e64fa6708d3d7ec65214687ab25869507c59
BenjaminBoisedu <benjaminboisedu354@gmail.com>
2023-11-29 12:21:07 +0100

    Add 3D

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -2,3 +2,42 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import gsap from "gsap";
+
+// Scene
+
+const scene = new THREE.Scene();
+
+// Camera
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.setZ(0);
+camera.position.setX(0);
+camera.position.setY(0);
+
+// Renderer
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg"),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
+
+// Torus
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({ color: 0x000000 });
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+// Lights
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight);
