0c2c024c31f838b10b635f267585a3efe87f56fa
BenjaminBoisedu <benjaminboisedu354@gmail.com>
2023-12-01 17:13:51 +0100

    Setup Vercel

--- Js/Dev.js
+++ Js/Dev.js
@@ -1,4 +1,4 @@
-import "../style.css";
+import "../Public/Css/Dev.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import gsap from "gsap";
