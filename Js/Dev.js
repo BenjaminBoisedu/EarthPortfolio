18788175aa8f50e076f2630919815f8a9a59ab7d
BenjaminBoisedu <benjaminboisedu354@gmail.com>
2023-12-01 14:48:16 +0100

    Start Dev Page

--- Js/Dev.js
+++ Js/Dev.js
@@ -3,6 +3,8 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import gsap from "gsap";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import MoonMap from "../Map-Planet/Moon.jpg";
+import BumpMoon from "../Map-Planet/BumpMoon.jpg";
 // Scene
 
 const scene = new THREE.Scene();
@@ -20,7 +22,7 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
+camera.position.setZ(20);
 camera.position.setX(10);
 renderer.render(scene, camera);
 
@@ -35,22 +37,23 @@ window.addEventListener("resize", resisze);
 
 // Sphere
 
-const SphereGeometry = new THREE.SphereGeometry(15, 32, 32);
+const SphereGeometry = new THREE.SphereGeometry(8, 128, 128);
 const SphereMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
+SphereMaterial.map = new THREE.TextureLoader().load(MoonMap);
+SphereMaterial.bumpMap = new THREE.TextureLoader().load(BumpMoon);
+SphereMaterial.bumpScale = 1;
+
 const Sphere = new THREE.Mesh(SphereGeometry, SphereMaterial);
 scene.add(Sphere);
 
 Sphere.position.z = 0;
-Sphere.position.setX(0);
-Sphere.position.y = -10;
+Sphere.position.setX(10);
+Sphere.position.y = 0;
 
 // Lights
 
-const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(ambientLight);
-
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(5, 5, 5);
+const pointLight = new THREE.DirectionalLight(0xffffff, 0.5);
+pointLight.position.set(-10, 10, 5);
 scene.add(pointLight);
 
 // Helpers
@@ -61,6 +64,10 @@ scene.add(lightHelper, gridHelper);
 
 function animate() {
   requestAnimationFrame(animate);
+
+  Sphere.rotation.x += 0.001;
+  Sphere.rotation.y += 0.001;
+  Sphere.rotation.z += 0.001;
 
   renderer.render(scene, camera);
 }
